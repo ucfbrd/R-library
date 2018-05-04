5665fedb9b5eb7fec9f927cc97160eda1045a6b8
Youssef Berrada <yberrada@mit.edu>
2018-05-04 15:53:33 -0400

    initial commit

--- list.R
+++ list.R
@@ -1,18 +1,17 @@
 # Install Packages by running the following command lines
 
 install.packages('data.table')
-install.packages(stargazer)
-install.packages(ggplot2)
-install.packages(quantmod)
-install.packages(dplyr)
-install.packages(stats)
-install.packages(ggplot2)
-install.packages(xts)
-install.packages(lubridate)
-install.packages(mice)
-install.packages(leaps)
-install.packages(foreign);
-install.packages(xtable);
-install.packages(plm);
-install.packages(gmm);
-install.packages(readstata13);
+install.packages('stargazer')
+install.packages('ggplot2')
+install.packages('quantmod')
+install.packages('dplyr')
+install.packages('stats')
+install.packages('xts')
+install.packages('lubridate')
+install.packages('mice')
+install.packages('leaps')
+install.packages('foreign')
+install.packages('xtable')
+install.packages('plm')
+install.packages('gmm)'
+install.packages('readstata13')
